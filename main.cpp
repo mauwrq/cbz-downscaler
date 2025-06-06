c1e64980dd3a771970a6ca6050782cf992029847
marc <raccoon@tuta.com>
2025-06-06 15:57:49 -0700

    feat: extract first page

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <zip.h>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
   zip *cbz;
@@ -10,6 +11,17 @@ int main(int argc, char* argv[]) {
   if ((cbz = zip_open(cbz_input, 0, &err)) == NULL) { // TODO: proper error checking
     return 0;
   }
+  
+  // TODO: put this in its own file
+  struct zip_stat st;
+  zip_stat_init(&st);
+  zip_stat_index(cbz, 0, 0, &st);
+  char *contents = new char[st.size];
+  zip_file *f = zip_fopen_index(cbz, 0, 0);
+  zip_fread(f, contents, st.size);
+  std::ofstream file("cover.jpg");
+  file.write(contents, st.size);
+  file.close();
   zip_close(cbz);
 }
 
