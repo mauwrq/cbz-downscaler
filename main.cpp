1d3bf22af659e96a16e9d8b6d157abf4d38d3f6c
marc <raccoon@tuta.com>
2025-06-07 15:28:44 -0700

    feat: convert image to jpg

--- main.cpp
+++ main.cpp
@@ -24,6 +24,18 @@ void scale_folder(fs::path folder_path, fs::path output_path, double width) {
   }
 }
 
+void to_jpg(fs::path img_path, fs::path output_path) {
+  std::string ext = img_path.extension().string();
+  std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+  if (ext != ".jpg") {
+    fs::path jpg_path = output_path / img_path.filename().replace_extension(".jpg");
+    std::ofstream file(jpg_path);
+    cv::Mat non_jpg = cv::imread(img_path);
+    cv::imwrite(jpg_path, non_jpg, std::vector<int>{cv::IMWRITE_JPEG_QUALITY, 95});
+    file.close();
+  }
+}
+
 void unzip(const char *zip_path, fs::path output_path) {
   zip *zip;
   int err = 0;
