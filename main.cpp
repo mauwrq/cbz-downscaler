f1247e776c289920d279a32b1d2314fc50b84339
marc <raccoon@tuta.com>
2025-06-06 21:48:34 -0700

    refactor: unzip function

--- main.cpp
+++ main.cpp
@@ -6,31 +6,34 @@
 
 namespace fs = std::filesystem;
 
+void unzip(const char *zip_path, fs::path output_path) {
+  zip *zip;
+  int err = 0;
+  if ((zip = zip_open(zip_path, 0, &err)) == NULL) { // TODO: proper error checking
+    return;
+  }
+
+  int image_count = zip_get_num_entries(zip, 0);
+  struct zip_stat st;
+  zip_stat_init(&st);
+
+  for (int i = 0; i < image_count; i++) {
+    zip_stat_index(zip, i, 0, &st);
+    char *contents = new char[st.size];
+    zip_file *f = zip_fopen_index(zip, i, 0);
+    zip_fread(f, contents, st.size);
+    std::ofstream file(output_path / st.name);
+    file.write(contents, st.size);
+    file.close();
+  }
+  zip_close(zip);
+}
+
 int main(int argc, char* argv[]) {
   fs::path executable_path = fs::canonical(argv[0]);
   fs::path project_root = executable_path.parent_path().parent_path();
-
-  zip *cbz;
-  const char *cbz_input;
-  cbz_input = argv[1];
-  int err = 0;
-  if ((cbz = zip_open(cbz_input, 0, &err)) == NULL) { // TODO: proper error checking
-    return 0;
-  }
-  
-  // TODO: put this in its own file
-  struct zip_stat st;
-  zip_stat_init(&st);
-  zip_stat_index(cbz, 0, 0, &st);
-  char *contents = new char[st.size];
-  zip_file *f = zip_fopen_index(cbz, 0, 0);
-  zip_fread(f, contents, st.size);
-
-  fs::create_directory(project_root / "output");
   fs::path output_path = project_root / "output";
-  std::ofstream file(output_path / "cover.jpg");
-  file.write(contents, st.size);
-  file.close();
-  zip_close(cbz);
+  const char *cbz_input = argv[1];
+  unzip(cbz_input, output_path);
 }
 
