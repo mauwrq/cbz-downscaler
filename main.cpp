fdfa766ece38d732812ee40197a928498ce3d68a
marc <raccoon@tuta.com>
2025-06-10 18:31:11 -0700

    feat: added webp conversion

--- main.cpp
+++ main.cpp
@@ -45,6 +45,27 @@ void to_jpgs(fs::path folder_path, fs::path output_path) {
   }
 }
 
+void to_webp(fs::path img_path, fs::path output_path) {
+  std::string ext = img_path.extension().string();
+  std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+  if (ext != ".webp") {
+    fs::path webp_path = output_path / img_path.filename().replace_extension(".webp");
+    std::ofstream file(webp_path);
+    cv::Mat non_jpg = cv::imread(img_path);
+    cv::imwrite(webp_path, non_jpg, std::vector<int>{cv::IMWRITE_WEBP_QUALITY, 95});
+    file.close();
+  }
+  else {
+    fs::copy_file(img_path, output_path / img_path.filename());
+  }
+}
+
+void to_webps(fs::path folder_path, fs::path output_path) {
+  for (auto const& img : fs::directory_iterator{folder_path}) {
+    to_webp(img.path(), output_path);
+  }
+}
+
 void unzip(const char *zip_path, fs::path output_path) {
   zip *zip;
   int err = 0;
@@ -99,8 +120,8 @@ int main(int argc, char* argv[]) {
   fs::path cbz_name = cbz_input;
   cbz_name.filename().replace_extension(".cbz");
   unzip(cbz_input, unzipped_path);
-  scale_imgs(unzipped_path, scaled_path, 720);
-  to_jpgs(scaled_path, converted_path);
+  scale_imgs(unzipped_path, scaled_path, 1264);
+  to_webps(scaled_path, converted_path);
   rezip(converted_path, output_path, cbz_name);
 }
 
